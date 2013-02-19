efe92e6e94f80f36b33a630d9eb4c75c8e35fdc5
Paul Miller <paul@paulmillr.com>
2013-02-19 07:31:44 +0200

    Update libs.

--- config.coffee
+++ config.coffee
@@ -11,7 +11,7 @@ exports.config =
         # Files in `vendor` directories are compiled before other files
         # even if they aren't specified in order.before.
         before: [
-          'vendor/scripts/console-helper.js',
+          'vendor/scripts/console-polyfill.js',
           'vendor/scripts/jquery-1.9.1.js',
           'vendor/scripts/underscore-1.4.4.js',
           'vendor/scripts/backbone-0.9.10.js'
--- vendor/scripts/console-polyfill.js
+++ vendor/scripts/console-polyfill.js
@@ -1,9 +1,11 @@
+// Console-polyfill. MIT license.
+// https://github.com/paulmillr/console-polyfill
 // Make it safe to do console.log() always.
 (function (con) {
   var method;
   var dummy = function() {};
   var methods = ('assert,count,debug,dir,dirxml,error,exception,group,' +
-     'groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,' + 
+     'groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,' +
      'time,timeEnd,trace,warn').split(',');
   while (method = methods.pop()) {
     con[method] = con[method] || dummy;
